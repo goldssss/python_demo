fec320c03f97e1210243e6b5377f9e2095189a5e
majinxin <63212640@qq.com>
2018-11-09 00:07:05 +0800

    页面爬取

--- demo_001/demo_django_001/views.py
+++ demo_001/demo_django_001/views.py
@@ -1,10 +1,17 @@
+import urllib
+
 from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse('hello world')
+    request.path='google.hk'
+    url = "https://www.google.com.hk/webhp?ie=UTF-8&rct=j"
+    req = urllib.request.Request(url)
+    resp = urllib.request.urlopen(req)
+    data = resp.read().decode('utf-8')
+    return HttpResponse(data)
 
 def hello(request):
     return render(request,"demo01.html")
